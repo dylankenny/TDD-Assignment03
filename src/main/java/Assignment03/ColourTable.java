162c0968844fab3248e169138c0c3e9590dcec29
dylankenny <dylankenny32@gmail.com>
2023-12-04 13:01:25 +0000

    Created simple code to pass test

--- src/main/java/Assignment03/ColourTable.java
+++ src/main/java/Assignment03/ColourTable.java
@@ -1,5 +1,15 @@
 package Assignment03;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ColourTable {
+    private int size;
+
+    public ColourTable(int size){
+        this.size = size;
+    }
+
+
 
 }
