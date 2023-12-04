a3dc0c22612e9af6575b8f5fca9141b696fbb86f
dylankenny <dylankenny32@gmail.com>
2023-12-04 13:20:23 +0000

    Created a Test to check if the colour added is a valid colour

--- src/test/java/ColourTableTest.java
+++ src/test/java/ColourTableTest.java
@@ -19,5 +19,12 @@ public class ColourTableTest {
         new ColourTable(3);
     }
 
+    @Test
+    public void checkIfColourIsValid(){
+        ColourTable table1 = new ColourTable(2);
+        table1.add(255,0,0);
+        table1.add(0,255,0);
+    }
+
 
 }
